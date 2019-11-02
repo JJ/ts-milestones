331daa139c3f504a5667f29e9de249159516a329
JJ Merelo <jjmerelo@gmail.com>
2019-11-02 15:02:38 +0100

    Passing tests

---
+++ src/Project.ts
@@ -0,0 +1,20 @@
+export enum State { Open,Closed };
+export class Issue {
+    private state: State = State.Open;
+    private project_name: string;
+    private id: number;
+    
+    constructor(project_name: string, id: number) {
+        this.project_name = project_name;
+	this.id = id;
+    }
+    
+    show_state() {
+	return this.state;
+    }
+    
+    close() {
+        this.state = State.Closed;
+    }
+}
+
--- src/__tests__/all_test.ts
+++ src/__tests__/all_test.ts
@@ -1,6 +1,17 @@
-import { Module } from '../Module';
+import { Issue, State } from '../Project';
+
+var data: Issue;
+
+beforeAll(() => {
+    data = new Issue("Foo",1);
+});
+
+
 
 test("all", () => {
-    const data = new Module.Class("new");
-    expect(  data.all() ).toBe("new");
+    console.log( data.show_state() );
+    expect(  data.show_state() ).toBe( State.Open );
+    data.close();
+    console.log( data.show_state() );
+    expect(  data.show_state() ).toBe( State.Closed );
 });
