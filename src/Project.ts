2b4684248d1140484188fa880a1e8665182178e9
JJ Merelo <jjmerelo@gmail.com>
2019-11-06 18:41:44 +0100

    Adds more tests

--- src/Project.ts
+++ src/Project.ts
@@ -1,20 +1,61 @@
-export enum State { Open,Closed };
-export class Issue {
-    private state: State = State.Open;
-    private project_name: string;
-    private id: number;
-    
-    constructor(project_name: string, id: number) {
-        this.project_name = project_name;
-	this.id = id;
-    }
-    
-    show_state() {
-	return this.state;
-    }
-    
-    close() {
-        this.state = State.Closed;
-    }
+export enum State {
+  Open,
+  Closed
 }
 
+export class Issue {
+  private state: State = State.Open;
+  private project_name: string;
+  private id: number;
+
+  constructor(project_name: string, id: number) {
+    this.project_name = project_name;
+    this.id = id;
+  }
+
+  show_state() {
+    return this.state;
+  }
+
+  close() {
+    this.state = State.Closed;
+  }
+
+  get_project_name() {
+    return this.project_name;
+  }
+}
+
+export class ProjectError extends Error {
+  constructor(public m?: string) {
+    super(m);
+    Object.setPrototypeOf(this, ProjectError.prototype);
+    this.name = "ProjectError";
+  }
+}
+
+export class Milestone {
+  private project_name: string;
+  private id: number;
+  private issues: Issue[] = [];
+
+  constructor(project_name: string, id: number) {
+    this.project_name = project_name;
+    this.id = id;
+  }
+
+  new_issue(issue: Issue) {
+    if (issue.get_project_name() != this.project_name) {
+      throw new ProjectError("when adding a new issue");
+    } else {
+      this.issues.push(issue);
+    }
+  }
+
+  get_project_name() {
+    return this.project_name;
+  }
+  get_issues() {
+    return this.issues;
+  }
+}
--- src/__tests__/all_test.ts
+++ src/__tests__/all_test.ts
@@ -1,15 +1,27 @@
-import { Issue, State } from '../Project';
+import { Issue, Milestone, ProjectError, State } from "../Project";
 
 var data: Issue;
+var milestone: Milestone;
+const project_name: string = "Foo";
 
 beforeAll(() => {
-    data = new Issue("Foo",1);
+  data = new Issue(project_name, 1);
+  milestone = new Milestone(project_name, 1);
 });
 
+test("Issue", () => {
+  expect(milestone.get_project_name()).toBe("Foo");
+  milestone.new_issue(data);
+  expect(data.show_state()).toBe(State.Open);
+  data.close();
+  expect(data.show_state()).toBe(State.Closed);
+});
 
-
-test("all", () => {
-    expect(  data.show_state() ).toBe( State.Open );
-    data.close();
-    expect(  data.show_state() ).toBe( State.Closed );
+test("Milestone", () => {
+  expect(milestone.get_project_name()).toBe("Foo");
+  expect(milestone.get_issues()).toHaveLength(1);
+  const new_issue = new Issue("Bar", 33);
+  expect(() => {
+    milestone.new_issue(new_issue);
+  }).toThrow(ProjectError);
 });
